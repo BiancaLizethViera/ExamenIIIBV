f257f5e952479f52e20fef15c3f5ed7af7c5c4d5
BiancaLizethViera <vierabianca65@gmail.com>
2022-04-26 15:01:07 -0600

    Liiz Viera
    BV

---
+++ ExamenIII/Datos/Repositorios/UsuarioRepositorio.cs
@@ -0,0 +1,33 @@
+﻿using Datos.Interfaces;
+using Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.Repositorios
+{
+    public class UsuarioRepositorio : IUsuarioRepositorio
+    {
+        public Task<bool> Actualizar(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Eliminar(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Usuario>> GetUsuarios()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Nuevo(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
